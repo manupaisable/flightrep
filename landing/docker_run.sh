720192817c3c941f96934d6124498ca966d1c273
manupaisable <manuelpais@gmail.com>
2015-10-07 18:31:59 +0200

    now mounting source as a volume so that changes not requiring any compilation are immediately visible without the need to re-run container

--- landing/docker_run.sh
+++ landing/docker_run.sh
@@ -1,2 +1,1 @@
-sudo docker build -t manupaisable/flightrep-landing .
-sudo docker run -it --rm -p 3000:3000 -p 3001:3001 --name run-flightrep-landing manupaisable/flightrep-landing
+sudo docker run -it --rm -p 3000:3000 -v $PWD:/usr/src/flightrep/landing --name run-flightrep-landing manupaisable/flightrep-landing
